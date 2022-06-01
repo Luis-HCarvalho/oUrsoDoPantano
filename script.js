7fbab82181a52bef77e4940d996d21e714a410dc
luis H <luishenriquecm12@gmail.com>
2022-06-01 01:50:06 -0300

    some changes

--- script.js
+++ script.js
@@ -1,41 +1,51 @@
-//change display from screen1 to screen2
+
+// set player leaderboards name and score
+var playerNameStatus = 0;
+function players(event){
+    let playerName = document.getElementById("inputPlayer");
+    //let playerScore; // id of the tag list in the html
+    //let scoreLi; // used to create li tags 
+    if (gameStartButton === 1){
+        if (playerName.value.length >= 3){
+            playerNameStatus = 1;
+            // achar uma forma de alterar o score "0"
+            localStorage.setItem(playerName.value, 0);
+            //create an elements li with the name and score in the pontuacao screen
+            // problema na criação de tags li
+            for (let i = 0; i < localStorage.length; i++){
+                let playerScore = document.getElementById("playerScore");
+                let scoreLi = document.createElement("li");
+                
+                scoreLi.textContent = localStorage.key(i) + " : " + localStorage.getItem(playerName);
+                
+                
+                playerScore.appendChild(scoreLi);
+
+                
+
+                scoreLi.value = "";
+                
+            }
+        }
+        else{
+            alert("O nome do jogador deve ter ao menos 3 letras");
+        }
+        playerName.value = "";
+    }
+}
+
+// change display from screen1 to screen2
 function changeScreen(event, screen1, screen2){
     screen1.style.display= "none";
     screen2.style.display = "block";
 }
 
 
-/*function scoreScreen(event, pName){
-    let elPlayerScore= document.getElementById("playerScore");
-    let listOfPlayers = document.createElement("li");
-    let pScore = String(pName);
-    listOfPlayers.appendChild(pScore);
-}*/
+// função de ordenação das pontuações 
 
 
-var score = [];
-var scoreIndex = 0;
-var playerNameStatus = 0;
-function players(event){
-    let playerName = document.getElementById("inputPlayer");
-    score[scoreIndex] = new Array(2);
-    if (event.keyCode === 13 || gameStartButton === 1){
-        if (playerName.value.length >= 3){
-            score[scoreIndex][0] = playerName;
-            scoreIndex++;
-            playerNameStatus = 1;
-            changeScreen(event, gameScreen1, gameScreen2);
-        }
-        else{
-            alert("O nome do jogador deve ter ao menos 3 letras");
-        }
-        alert(playerName.value);
-        alert("scoreIndex: " + scoreIndex);
-        playerName.value = "";
-    }
-}
 
-
+// fazer o botao de começar o jogo funcionar como um submit do nome do jogador
 var gameStartButton = 0;
 function gameStart(event){
     gameStartButton = 1;
@@ -50,6 +60,7 @@ var proficiency;
 function submit(event, inputN){
     let input = document.getElementById(inputN);
     if (event.keyCode === 13){
+        soundEffects(event, "inputSound");
         if ((input.value != '1') && (input.value != '2') && (input.value != '3') && (input.value != '4')){
             alert("Escolha invalida... tente  de novo!");
         }
@@ -68,4 +79,21 @@ function submit(event, inputN){
         input.value = "";
     }
     
+}
+
+//sound effects for buttons and inputs in the game
+function soundEffects(event, sound){
+    let audio;
+    if (sound === "hoverSound"){
+        audio = new Audio('mixkit-video-game-mystery-alert-234.wav');
+        audio.play();
+    }
+    else if (sound === "clickSound"){
+        audio = new Audio('mixkit-retro-game-notification-212.wav');
+        audio.play();
+    }
+    else if (sound === "inputSound"){
+        audio = new Audio('mixkit-arcade-bonus-alert-767.wav');
+        audio.play();
+    }
 }
